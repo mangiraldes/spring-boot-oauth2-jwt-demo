1938c14c302c9231ecedb3d04f32895538cb514a
manoel giraldes <manogiraldes@gmail.com>
2025-01-01 14:52:41 -0300

    interfaces de segurança

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,22 @@
 package com.devsuperior.demo;
 
+import com.devsuperior.demo.config.SecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.Console;
 
 @SpringBootApplication
 public class DemoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
+
 
 }
